03c1f7ca8b32a1bdbe4316c756c8b4bc7fe2881c
Nirali <nirali.mehta112@gmail.com>
2025-03-17 18:01:16 -0700

    added environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB2lH_ZyhWzQGamZxwIC5zHJyr_3UW94XE",
+    authDomain: "subscription-tracker-39050.firebaseapp.com",
+    projectId: "subscription-tracker-39050",
+    storageBucket: "subscription-tracker-39050.appspot.com",
+    messagingSenderId: "464667715284",
+    appId: "1:464667715284:web:2cdef19a4e9f5772373d41",
+    measurementId: "G-PK7VZMBYD6"
+  },
+};
